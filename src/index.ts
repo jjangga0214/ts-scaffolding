a7c4e6a96a5301835f6d471d50f522470a1a8d35
jjangga0214 <bnbcmindnpass@gmail.com>
2022-02-15 21:08:16 +0900

    chore: init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-export * from '#/adder'
+import publicIp from 'public-ip'
+import { internalIpV6, internalIpV4 } from 'internal-ip'
+
+async function main() {
+  console.log(`public ip v4: ${await publicIp.v4()}`)
+  //= > '46.5.21.123'
+
+  // console.log(`public ip v6: ${await publicIp.v6()}`)
+  //= > 'fe80::200:f8ff:fe21:67cf'
+
+  console.log(`private ip v4: ${await internalIpV4()}`)
+  //= > '10.0.0.79'
+
+  console.log(`private ip v6: ${await internalIpV6()}`)
+  //= > 'fe80::1'
+}
+
+main()
