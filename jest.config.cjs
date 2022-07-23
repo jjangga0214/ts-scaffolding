b0e5f59661862f04deeac841ba209088ce878896
jjangga0214 <bnbcmindnpass@gmail.com>
2022-07-23 14:17:01 +0900

    test(config): fix import

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,12 +1,10 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-const { getConfig } = require('@jjangga0214/jest-config')
+const { produceConfig } = require('@jjangga0214/jest-config')
 const { compilerOptions } = require('./tsconfig')
 
 module.exports = {
-  ...getConfig(compilerOptions),
-
-  // You can override other fields as well.
-  // transform: {
-  //   '.(ts|tsx)': 'ts-jest',
-  // },
+  ...produceConfig(compilerOptions),
+  transform: { '^.+\\.(t|j)sx?$': ['@swc/jest'] },
+  // transform: { '^.+\\.tsx?$': ['esbuild-jest'] },
+  // transform: { '^.+\\.tsx?$': ['ts-jest'] },
 }
